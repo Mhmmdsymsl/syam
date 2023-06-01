55923aa6c9011a2fcdc65a78071558a5760dae51
Mhmmdsymsl <mhmmdsymsl05@gmail.com>
2023-06-02 01:04:14 +0800

    update

--- views/mahasiswa022/view.php
+++ views/mahasiswa022/view.php
@@ -6,11 +6,11 @@ use yii\widgets\DetailView;
     DetailView::widget([
         'model' => $mhs,
         'attributes' => [
-            'id032',
-            'nim032',
-            'nama032',
-            'kelas032',
-            'status032',
+            'id',
+            'nim',
+            'nama',
+            'kelas',
+            'status',
         ],
     ]);
 ?>
